941ab519d20291957d5e5196d129018a13993742
Matt Zhang <mattzhang9020@gmail.com>
2024-06-08 16:59:40 +0800

    code update

--- modules/diffusion/trainer.py
+++ modules/diffusion/trainer.py
@@ -19,6 +19,16 @@ class DiffusionTrainer(nn.Module):
 
         self.register_buffer('sqrt_alphas_bar', torch.sqrt(alphas_bar))
         self.register_buffer('sqrt_one_minus_alphas_bar', torch.sqrt(1. - alphas_bar))
+    
+    def get_x_T(self, x_0):
+        t = x_0.new_ones([x_0.shape[0], ], dtype=torch.long) * (self.steps-1)
+                
+        noise = torch.randn_like(x_0)
+                
+        x_T = (extract(self.sqrt_alphas_bar, t, x_0.shape) * x_0 +
+               extract(self.sqrt_one_minus_alphas_bar, t, x_0.shape) * noise)
+        
+        return x_T
 
     def forward(self, x_0):
         t = torch.randint(self.steps, size=(x_0.shape[0], ), device=x_0.device)
